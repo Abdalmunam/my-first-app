95b3e5e16f78479efc7b7aef932cae10fedcaa47
Abdalmunam Abdalla <abdalmenam1980@gmail.com>
2022-02-08 20:58:30 +0200

    initial create, add Hello & Hellow components

---
+++ src/components/Hellow.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export class Hellow extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello React JS</h1>
+      </div>
+    )
+  }
+}
+
+export default Hellow;
