e4ac5f57393e5b82f426855dde9b30beea06df17
Abdalmunam Abdalla <abdalmenam1980@gmail.com>
2022-02-12 02:09:49 +0200

    other event bind methods

--- src/components/EventBind.js
+++ src/components/EventBind.js
@@ -7,18 +7,27 @@ export class EventBind extends Component {
     this.state = {
       message: 'Hello',
     }
+    this.clickHandler = this.clickHandler.bind(this)
   }
-  clickHandler() {
+//   clickHandler() {
+//     this.setState({
+//       message: 'Goodby',
+//     })
+//     console.log(this)
+//   }
+  clickHandler = () => {
     this.setState({
       message: 'Goodby',
     })
-    console.log(this)
   }
+
   render() {
     return (
       <div>
         <div>{this.state.message}</div>
-        <button onClick={this.clickHandler.bind(this)}>Click</button>
+        {/*<button onClick={this.clickHandler.bind(this)}>Click</button>*/}
+        {/*<button onClick={() => this.clickHandler()}>Click</button>*/}
+        <button onClick={this.clickHandler}>Click</button>
       </div>
     )
   }
